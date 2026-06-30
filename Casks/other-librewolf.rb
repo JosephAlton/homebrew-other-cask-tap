49e45ec80e226f2eb7a73091375b56c4875dbf4d
JosephAlton <45772406+JosephAlton@users.noreply.github.com>
2026-06-30 15:45:57 +0100

    add LibreWolf, slight mod to bypass gatekeeper

---
+++ Casks/other-librewolf.rb
@@ -0,0 +1,55 @@
+cask "other-librewolf" do
+    arch arm: "arm64", intel: "x86_64"
+  
+    version "152.0.2,1"
+    sha256 arm:   "8f959081bf450e22f3de3e78ebf133667aeb8aeff873fc300156e232da3ace2c",
+           intel: "86212ebd8ec663df57c774d84c4cb03ce0ebcc39a74a67332397e1f210c6da23"
+  
+    url "https://codeberg.org/api/packages/librewolf/generic/librewolf/#{version.tr(",", "-")}/librewolf-#{version.tr(",", "-")}-macos-#{arch}-package.dmg",
+        verified: "codeberg.org/api/packages/librewolf/generic/librewolf/"
+    name "LibreWolf"
+    desc "Web browser"
+    homepage "https://librewolf.net/"
+  
+    # There can be a notable gap between when a version is tagged and a
+    # corresponding release is created, so we check the "latest" release instead
+    # of the Git tags.
+    livecheck do
+      url "https://codeberg.org/api/v1/repos/librewolf/bsys6/releases/latest"
+      regex(/^v?(\d+(?:[.-]\d+)+)$/i)
+      strategy :json do |json, regex|
+        json["tag_name"]&.[](regex, 1)&.tr("-", ",")
+      end
+    end
+  
+    # disable! date: "2026-09-01", because: :fails_gatekeeper_check
+  
+    depends_on :macos
+  
+    app "LibreWolf.app"
+    # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+    shimscript = "#{staged_path}/librewolf.wrapper.sh"
+    binary shimscript, target: "librewolf"
+  
+    preflight do
+      File.write shimscript, <<~EOS
+        #!/bin/sh
+        exec '#{appdir}/LibreWolf.app/Contents/MacOS/librewolf' "$@"
+      EOS
+    end
+
+    # Added to bypass Gatekeeper
+    postflight do
+      system_command "/usr/bin/xattr",
+                     args: ["-dr", "com.apple.quarantine", "#{appdir}/LibreWolf.app"]
+    end
+
+    zap trash: [
+      "~/.librewolf",
+      "~/Library/Application Support/LibreWolf",
+      "~/Library/Caches/LibreWolf Community",
+      "~/Library/Caches/LibreWolf",
+      "~/Library/Preferences/io.gitlab.librewolf-community.librewolf.plist",
+      "~/Library/Saved Application State/io.gitlab.librewolf-community.librewolf.savedState",
+    ]
+  end
